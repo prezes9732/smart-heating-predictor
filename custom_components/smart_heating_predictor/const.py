539b4734219db4ee2178c52491f5c62bf870af63
prezes9732 <prezes9732@gmail.com>
2025-10-22 13:56:22 +0200

    Create const.py

---
+++ custom_components/smart_heating_predictor/const.py
@@ -0,0 +1,5 @@
+# Smart Heating Predictor Constants
+# Author: prezes9732
+# Full implementation at: https://www.perplexity.ai/search/stworz-mi-custom-integration-d-4WuaNPQWQ.u3BH4ciKFAtw
+
+DOMAIN = "smart_heating_predictor"
